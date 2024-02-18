538059e1d84501a581dfb166ce84f0a0e7442c93
Joshua Yustana <joshiosimoe@gmail.com>
2024-02-18 02:09:20 -0600

    Added NavBar Animations

--- app/components/navBar.tsx
+++ app/components/navBar.tsx
@@ -13,32 +13,32 @@ const NavBar = () => {
         <div className="w-[38px] h-[38px] bg-blue-600 rounded-[20px]"/>
 
         {/*Button with Text */}
-        <div className="w-24 h-[35px] bg-white rounded-[99px] border border-blue-600">
-            <div className="relative text-center text-blue-600 text-[20px] font-bold font-['Montserrat']">
+        <div className="w-24 h-[35px] bg-white rounded-[99px] border border-blue-600 hover:bg-blue-600">
+            <div className="relative text-center text-blue-600 text-[20px] font-bold font-['Montserrat'] hover:text-white">
             <Link href='/'>About</Link> 
             </div>
         </div>
 
-        <div className="w-24 h-[35px] bg-white rounded-[99px] border border-blue-600">
-            <div className="relative text-center text-blue-600 text-[20px] font-bold font-['Montserrat']">
+        <div className="w-24 h-[35px] bg-white rounded-[99px] border border-blue-600 hover:bg-blue-600">
+            <div className="relative text-center text-blue-600 text-[20px] font-bold font-['Montserrat'] hover:text-white">
             <Link href='/'>Connect</Link> 
             </div>
         </div>
 
-        <div className="w-24 h-[35px] bg-white rounded-[99px] border border-blue-600">
-            <div className="relative text-center text-blue-600 text-[20px] font-bold font-['Montserrat']">
+        <div className="w-24 h-[35px] bg-white rounded-[99px] border border-blue-600 hover:bg-blue-600">
+            <div className="relative text-center text-blue-600 text-[20px] font-bold font-['Montserrat'] hover:text-white">
             <Link href='/'>Career</Link> 
             </div>
         </div>
 
-        <div className="w-24 h-[35px] bg-white rounded-[99px] border border-blue-600">
-            <div className="relative text-center text-blue-600 text-[20px] font-bold font-['Montserrat']">
+        <div className="w-24 h-[35px] bg-white rounded-[99px] border border-blue-600 hover:bg-blue-600">
+            <div className="relative text-center text-blue-600 text-[20px] font-bold font-['Montserrat'] hover:text-white">
             <Link href='/'>Support</Link> 
             </div>
         </div>
 
-        <div className="w-24 h-[35px] bg-white rounded-[99px] border border-blue-600">
-            <div className="relative text-center text-blue-600 text-[20px] font-bold font-['Montserrat']">
+        <div className="w-24 h-[35px] bg-white rounded-[99px] border border-blue-600 hover:bg-blue-600">
+            <div className="relative text-center text-blue-600 text-[20px] font-bold font-['Montserrat'] hover:text-white">
             <Link href='/message'>Message</Link> 
             </div>
         </div>
@@ -48,14 +48,14 @@ const NavBar = () => {
             <div className="relative text-start px-1 text-black opacity-50 text-[20px] font-bold font-['Montserrat']">Search</div>
         </div>
 
-        <div className="w-24 h-[35px] bg-white rounded-[99px] border border-blue-600 col-start-11">
-            <div className="relative text-center text-blue-600 text-[20px] font-bold font-['Montserrat']">
+        <div className="w-24 h-[35px] bg-white rounded-[99px] border border-blue-600 col-start-11 hover:bg-blue-600">
+            <div className="relative text-center text-blue-600 text-[20px] font-bold font-['Montserrat'] hover:text-white">
             <Link href='/connect'>Log In</Link> 
             </div>
         </div>
 
-        <div className="w-24 h-[35px] bg-blue-600 rounded-[99px] col-start-12">
-            <div className="relative text-center text-white text-[20px] font-bold font-['Montserrat']">
+        <div className="w-24 h-[35px] bg-blue-600 rounded-[99px] border border-blue-600 col-start-12 hover:bg-white">
+            <div className="relative text-center text-white text-[20px] font-bold font-['Montserrat'] hover:text-blue-600">
             <Link href='/connect'>Sign In</Link> 
             </div>
         </div>
