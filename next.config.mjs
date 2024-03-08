abbf9454c5c4281a4f60ff6443f9d1ad352cac28
Jack Norris <jacktimothynorris@gmail.com>
2024-03-08 13:45:22 -0600

    added in CI/CD

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+import { basePath } from './helper.mjs';
+
+const nextConfig = {
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
+    reactStrictMode: true,
+    basePath,
+    assetPrefix: `${basePath}/`,
+    experimental: {
+        esmExternals: 'loose',
+    },
+    webpack: (config) => {
+        config.externals = [...config.externals, { canvas: 'canvas' }];
+        return config;
+    },
+};
+
+module.exports = nextConfig
